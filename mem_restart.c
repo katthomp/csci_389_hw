ba13be6196e28bed5c52a46522d71deacd675026
Kathy Thompson <kathythompson357@gmail.com>
2018-09-30 16:21:58 -0700

    ples

--- mem_restart.c
+++ mem_restart.c
@@ -25,8 +25,8 @@ float time_read_byte(char* order, int buffer_size){
 
 void run_many(){
     int NUMBER_RUNS = 10;
-    float average;
     for (int buffer_size=START_SIZE_BYTES; buffer_size<=END_SIZE_BYTES;buffer_size = buffer_size<<1){
+        float average=0;
         char* order = calloc(buffer_size,sizeof(char));
             if(!order) {
             perror("Didn't allocate memory correctly (order)");
