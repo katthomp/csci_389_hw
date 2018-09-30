0a83c60ea351912c6c6d2c4035afb6f1be217ce1
Kathy Thompson <kathythompson357@gmail.com>
2018-09-30 15:31:36 -0700

    this one worksgit add mem_restart.c! highest buffer size at 86 sec

---
+++ mem_restart.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+volatile char read;
+const int BILLION=1000000000;
+float time_read_byte(char* order, int buffer_size){
+    int r=0;
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+    for(int j=0;j<buffer_size;++j){
+        read=order[r];
+        r=(j+r+read)&buffer_size;
+    }
+    if (read==-1 || r==-1){
+        printf("This is not supposed to happen");
+    }
+    clock_gettime(CLOCK_MONOTONIC_RAW,&end);
+    float t=(BILLION*(end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec);
+    float time_per_byte = t/buffer_size;
+    return time_per_byte;
+} 
+
+void run_many(){
+    int NUMBER_RUNS = 10;
+    float average;
+    for (int buffer_size=1024; buffer_size<=67108864;buffer_size = buffer_size<<1){
+        char* order = calloc(buffer_size,sizeof(char));
+            if(!order) {
+            perror("Didn't allocate memory correctly (order)");
+            exit(-1);
+        }
+        char* buffer =calloc(buffer_size, sizeof(char));
+        if(!buffer) {
+        perror("Didn't allocate memory correctly (buffer)");
+        exit(-1);
+    }
+        for (int i=0; i<buffer_size; ++i){
+            float r=rand()%buffer_size;
+            order[i]=r;
+        }
+    
+        for (int i=0; i<NUMBER_RUNS; ++i){
+            average+=time_read_byte(order, buffer_size);   
+        }  
+        printf("buffer size: %d, average time: %f\n \n",buffer_size,average/NUMBER_RUNS); 
+    free(order);
+    free(buffer);
+}
+return;}
+int main(){
+    run_many();
+    
+    
+    return 0;
+}
