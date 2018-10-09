a11a402c088aa702bdbccfc5edaf7978f79f7aca
Kathy Thompson <kathythompson357@gmail.com>
2018-10-09 15:59:52 -0700

    lol merge

--- cache.cc
+++ cache.cc
@@ -2,24 +2,37 @@
 
 
 */
-
 #include "cache.hh"
-
+#include <unordered_map>
 struct Cache::Impl{
     index_type maxmem=32*sizeof(char); //starting by implementing a Very Small Cache for Testing
+    index_type memused=0;
+    std::unordered_map<std::string, void*> map_;
     void del(key_type key){
-
+        map_.erase(key);
+        return;
     }
-    val_type get(key_type key, index_type& val_size)const{
-
+    val_type get(key_type key, index_type& val_size) const{
+        return map_.find(key);
     }
     index_type space_used()const{
-
+        return memused;
     }
     void set(key_type key, val_type value, index_type size){
-
+        if (size>maxmem){
+            printf("This size cannot fit in this cache");
+            exit(-1);
+        }
+        //making sure that the size can fit in the cache
+        if ((size+memused)<maxmem){
+            map_[key]=value;
+            memused+=size;
+            maxmem-=size;
+            return;
+        }
     }
-    key_type key=0;
+    
+
 };
 
 //* Retrieve a pointer to the value associated with key in the cache,
@@ -27,22 +40,24 @@ struct Cache::Impl{
 //* Sets the actual size of the returned value (in bytes) in val_size.
 Cache::Cache(index_type maxmem, evictor_type evictor, hash_func hasher)
     :pImpl_(new Impl())
-
+{
+    
+}
 
 Cache::val_type Cache::get(key_type key, index_type& val_size)const{
     //need to point to a pointer
-    pImpl_->key;
-    return 0;
+    return pImpl_->get(key, val_size);
 }
 //* Compute the total amount of memory used up by all cache values (not keys)
 Cache::index_type Cache::space_used()const{
     //unsure of how it's pointing to all of the places in memory
     //very unsure of how to traverse this
-    pImpl_->space_used();
-    return 0;
+    return pImpl_->space_used();
+
 }
 //* Delete an object from the cache, if it's still there
 void Cache::del(key_type key){
+    pImpl_->del(key);
     return;
 }
 //* Add a <key, value> pair to the cache.
@@ -51,11 +66,7 @@ void Cache::del(key_type key){
 //* If maxmem capacity is exceeded, sufficient values will be removed
 //* from the cache to accomodate the new value.
 void Cache::set(key_type key, val_type value, index_type size){
-    // if (pImpl_->space_used())>=pImpl_->maxmem{
-    //     for pImpl_->space_used()>=maxmem{
-    //         pImpl_->del(key_type key);
-    //     }
-    // }
+    pImpl_->set(key,value,size); 
     return;
 }
 
